617c9bdaa921c136acdbf8debaa8d70544880c11
cckn <cckn1985@gmail.com>
2019-09-29 06:26:22 +0900

    Markup components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import TodoTemplate from './components/TodoTemplate'
+import TodoInsert from './components/TodoInsert'
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
-  return <div>Let's make Todo App!</div>
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList></TodoList>
+    </TodoTemplate>
+  )
 }
 
 export default App
