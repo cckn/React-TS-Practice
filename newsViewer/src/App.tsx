31fd9363136208a95667c38024b96642f50a86d0
cckn <cckn1985@gmail.com>
2019-09-30 07:00:33 +0900

    [Learn] use Axios - then

--- newsViewer/src/App.tsx
+++ newsViewer/src/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import axios from 'axios'
 const App: React.FC = () => {
-  return <div className="App">Learning React</div>
+  const [data, setData] = useState(null)
+
+  const onClick = () => {
+    axios
+      .get('http://jsonplaceholder.typicode.com/todos/1')
+      .then((responce) => {
+        setData(responce.data)
+      })
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        ></textarea>
+      )}
+    </>
+  )
 }
 
 export default App
